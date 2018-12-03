d00ca9c5b053b9fa625ded47f5f7f0676c5f1c4f
Luke <jakielaszekluke@gmail.com>
2018-12-03 15:03:33 -0500

    added in a EOF CHAR functionality

--- main.c
+++ main.c
@@ -12,10 +12,11 @@
 
 // filesystem information
 #define DISK_SIZE 100 // size in bytes of filesystem
-#define BLOCK_SIZE 3 // size in bytes of blocks
+#define BLOCK_SIZE 5 // size in bytes of blocks
 #define INDICE_SIZE 8 // size in bytes of index
 #define OPEN_INDEX -1 // indicates unused index location
 #define EOF_INDEX -2 // indicates file ends at index location
+#define EOF_CHAR 34 // character to signal final char in file
 #define FILE_TYPE 70 // signifies file is a file F:70
 #define DIRECTORY_TYPE 68 // signifies file is a dir D:68
 #define META_SIZE 21 // size in bytes of meta section
@@ -59,7 +60,9 @@ void writeData(int blockIndex, int offset, int curChar, char * data,
 void updateIndex(int indexLocation, int nextIndex, struct Indices * indices);
 int readData(int blockIndex, int offset, int count, char * buffer,
 	     int buffsize, struct Blocks * blocks, struct Indices * indices);
-//int isPathValid(char * path, struct Indices * indices, struct Blocks * blocks);
+int isPathValid(char * path, struct Indices * indices, struct Blocks * blocks);
+char * readFile(int startBlock, int offset, struct Blocks * blocks,
+	       struct Indices * indices);
 
 int main(){
   // check validity of entered parameters
@@ -91,15 +94,17 @@ int main(){
   // creates a filesystem mapping to memory
   mapFileSystem(&blocks, &indices);
 
-  createDir("", indices, blocks);
-
   char str[80] = "/hello/world/boy.txt";
 
   char * buf = (char*)malloc(sizeof(char)*50);
   
   int count = readData(0,0, 0, buf, 50, blocks, indices);
 
-  printf("%s:%d\n", buf, count);
+  char path[] = "/data/isip/luke.txt";
+  
+  isPathValid(path, indices, blocks);
+
+  munmap(indices, ALLOC_DISK_SPACE);
   
   return 0;
 }
@@ -110,11 +115,7 @@ struct LFILE * createDir(char * dirName,
   // find and claim the next open block
   int open = getOpenBlock(indices, blocks);
 
-  printf("available block at [%d]\n", open);  
-
   char * meta = createMeta(DIRECTORY_TYPE);
-
-  printf("[%s]\n", meta);
 
   // checks if root directory
   if(open == 0){
@@ -125,38 +126,78 @@ struct LFILE * createDir(char * dirName,
 	    dirName);
     exit(EXIT_FAILURE);
   }else{
-
+    
   }
 }
 
-/*
 // checks if path exists, returns 1 on success 0 on failure
 int isPathValid(char * path, struct Indices * indices, struct Blocks * blocks){
+  // check if path is empty
+  if(path == NULL){
+    return 0;
+  }
+  
   // ensures absolute path
   if(path[0] != '/'){
     return 0;
   }
 
-  char delim[2] = "/";
+  char delim[] = "/";
   char * token;
+
+  int startBlock = 0;
+  int offset = 0;
+
+  char * total = readFile(startBlock, offset, blocks, indices);
+
+  printf("[%s]\n", total);
   
+  /*
   token = strtok(path, delim);
-  
+
   while(token != NULL){
+    printf("%s\n", token);
     token = strtok(NULL, delim);
   }
-
+  */
   return 1;
 }
 
-int getBlock(char * data, int start, struct Indices * indices,
-	     struct Blocks * blocks){
-  int blockIndex = -1;
-  if(){
+char * readFile(int startBlock, int offset, struct Blocks * blocks,
+	       struct Indices * indices){
+  // temporary buffer for reading sections
+  int buffsize = 50;
+  char * buffer = (char*)malloc(sizeof(char)*buffsize);
 
-  }
+  // total size of directory
+  int totalSize = buffsize;
+  char * total = (char*)malloc(sizeof(char)*totalSize);
+  total[0] = '\0';
+  
+  int count = 0;  
+  // read directory until end
+  do{
+    // partially read directory
+    count = readData(startBlock, offset, 0, buffer, buffsize,
+	     blocks, indices);
+
+    // increment offset
+    offset += count;
+
+    // if total fills up, realloc more space
+    if(offset <= totalSize){
+      totalSize *= 2;
+      total = (char*)realloc(total, sizeof(char)*totalSize);
+    }
+
+    // concatenate buffer into total
+    strcat(total, buffer);
+  }while(count == buffsize-1);
+
+  free(buffer);
+  
+  return total;
 }
-*/
 
 // reads data until buffer is full or file ends, returns number of characters
 // read
@@ -165,6 +206,11 @@ int readData(int blockIndex, int offset, int count, char * buffer,
   // reads data to buffer at char offset
   int i = offset;
   for(i = offset; count < buffsize-1; i++, count++){
+    // if hit end of file
+    if(blocks[blockIndex].data[i] == EOF_CHAR){
+      break;
+    }
+    
     // if block is full
     if(i >= BLOCK_SIZE){
       // get value in indice
@@ -191,7 +237,7 @@ int readData(int blockIndex, int offset, int count, char * buffer,
     buffer[count] = blocks[blockIndex].data[i];
   }
 
-  // add newline to end of buffer
+  // add null to end of buffer
   buffer[count] = '\0';
 
   // return total number of chars read
@@ -273,7 +319,7 @@ int getOpenBlock(struct Indices * indices, struct Blocks * blocks){
     char buffer[INDICE_SIZE];
 
     // check if index is available
-    if(hexToInt(indices[index]) == OPEN_INDEX){
+    if(hexToInt(indices[index]) == OPEN_INDEX){  
       retIndex = index;
       break;
     }
@@ -311,19 +357,20 @@ void claimIndex(struct Indices * indices, int openIndex){
 // converts hex to int from indice struct data
 int hexToInt(struct Indices index){
   char buffer[INDICE_SIZE];
-
+  
   // store index value in buffer
   int i = 0;
-  for(i = 0; i < NUM_BLOCKS-1; i++){
+  for(i = 0; i < INDICE_SIZE; i++){
     buffer[i] = index.data[i];
   }
 
   buffer[i] = '\0';
-
+  
   // convert hex chars to int
   return (int)strtol(buffer, NULL, 16);
 }
 
+// mmap filesystem file to memory
 void mapFileSystem(struct Blocks ** blocks, struct Indices ** indices){
   int fd;
   struct stat stat_buff;
@@ -376,10 +423,21 @@ void createFileSystem(){
   }
 
   for(i = 0; i < DATA_SIZE; i++){
-    fprintf(f, "0");
+    fprintf(f, "%c", EOF_CHAR);
   }
   
   fclose(f);
+
+  struct Blocks * blocks; // block pointers
+  struct Indices * indices; //indice pointers
+
+  // creates a filesystem mapping to memory
+  mapFileSystem(&blocks, &indices);
+
+  createDir("", indices, blocks);
+
+  // unmap filesystem
+  munmap(indices, ALLOC_DISK_SPACE);
   
   printf("File System Constructed\n\n");
 }
